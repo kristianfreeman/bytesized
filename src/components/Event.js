1e44837401070d6a06bcf63215c88102604973ee
Kristian Freeman <kristian@kristianfreeman.com>
2018-12-06 09:14:53 -0700

    More positioning

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,44 +1,58 @@
 import React from 'react'
+import LazyLoad from 'react-lazyload'
+
+const s3Url = path => `https://byteconf-production.s3.amazonaws.com/${path}`
+
+const Container = ({ children, eventType, url }) => (
+  <div
+    className={`w-full mb-4 md:flex-${
+      eventType === 'conference' ? 2 : 1
+    } mr-2 rounded overflow-hidden shadow-lg`}
+    style={{
+      background: `url(${url}), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))`,
+      backgroundSize: 'cover',
+    }}
+  >
+    {children}
+  </div>
+)
 
 const Event = ({ event }) => {
   const parsedDate = new Date(event.start_date)
   return (
-    <div
-      className="lazy w-full mb-4 md:flex-1 mr-2 rounded overflow-hidden shadow-lg"
-      data-bg="url(<%= S3Image.image_url(event.cover_path) %>)"
-      style={{
-        background: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className="px-6 py-4 flex flex-col h-full">
-        <div className="font-bold text-2xl mb-2 text-white">
-          <i className="mr-1 fa <%= event.icon %>" />
-          <a className="text-white" href={event.url}>
-            {event.name}
-          </a>
-        </div>
-        {Date.parse(event.start_date) > Date.now() && (
-          <p className="text-white text-lg pt-4 pb-6 flex-grow">
-            {event.description}
-          </p>
-        )}
-        <p className="text-white text-lg font-bold">
-          {parsedDate.toLocaleDateString('en-US')}
-        </p>
-        {event.youtube_playlist && (
-          <p className="text-white text-lg font-bold mt-8 mb-2">
-            <a
-              href={event.youtube_playlist}
-              className="bg-green p-2 no-underline text-white"
-            >
-              <i className="fab fa-twitch mr-1" />{' '}
-              <span className="hover:underline">Watch now</span>
+    <LazyLoad>
+      <Container eventType={event.event_type} url={s3Url(event.cover_path)}>
+        <div className="px-6 py-4 flex flex-col h-full">
+          <div className="font-bold text-2xl mb-2 text-white">
+            <i className="mr-1 fa <%= event.icon %>" />
+            <a className="text-white" href={event.url}>
+              {event.name}
             </a>
+          </div>
+          {Date.parse(event.start_date) > Date.now() && (
+            <p className="text-white text-lg pt-4 pb-6 flex-grow">
+              {event.description}
+            </p>
+          )}
+          <p className="text-white text-lg font-bold">
+            {parsedDate.toLocaleDateString('en-US')}
           </p>
-        )}
-      </div>
-    </div>
+          {event.youtube_playlist && (
+            <p className="text-white text-sm mt-8 mb-2">
+              <a
+                href={event.youtube_playlist}
+                className="no-underline text-white"
+              >
+                <i className="fab fa-youtube mr-1 text-white" />{' '}
+                <span className="text-white no-underline hover:underline">
+                  Watch on YouTube
+                </span>
+              </a>
+            </p>
+          )}
+        </div>
+      </Container>
+    </LazyLoad>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,72 @@ import Event from '../components/Event'
 import Nav from '../components/Nav'
 import Layout from '../components/Layout'
 
+const TODAY = new Date()
+
+const Header = () => (
+  <div className="py-4">
+    <h2 className="text-3xl font-normal py-4">
+      Free developer conferences for everyone, streamed on Twitch.
+    </h2>
+
+    <p>
+      <a
+        className="no-underline text-black"
+        href="/s/blog"
+        title="Byteconf Blog"
+      >
+        <i className="fas fa-newspaper fa-2x border-white pr-4" />
+      </a>
+      <a
+        className="no-underline text-black"
+        href="/s/newsletter"
+        title="Byteconf Newsletter"
+      >
+        <i className="fas fa-envelope fa-2x border-white p-4" />
+      </a>
+      <a
+        className="no-underline text-black"
+        href="/s/twitter"
+        title="@byteconf on Twitter"
+      >
+        <i className="fab fa-twitter fa-2x no-underline border-white p-4" />
+      </a>
+      <a
+        className="no-underline text-black"
+        href="/s/discord"
+        title="Byteconf Discord channel"
+      >
+        <i className="fab fa-discord fa-2x border-white p-4" />
+      </a>
+      <a
+        className="no-underline text-black"
+        href="/s/twitch"
+        title="@byteconf on Twitch"
+      >
+        <i className="fab fa-twitch fa-2x border-white p-4" />
+      </a>
+      <a
+        className="no-underline text-black"
+        href="/s/youtube"
+        title="@byteconf on YouTube"
+      >
+        <i className="fab fa-youtube fa-2x border-white p-4" />
+      </a>
+    </p>
+  </div>
+)
+
+const afterToday = date => new Date(date) > TODAY
+
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const siteDescription = data.site.siteMetadata.description
     const events = get(data, 'sanity.allEvents', [])
+
+    const upNext = events.filter(({ start_date }) => afterToday(start_date))
+    const previous = events.filter(({ start_date }) => !afterToday(start_date))
 
     return (
       <Layout>
@@ -23,15 +83,33 @@ class BlogIndex extends React.Component {
           title={siteTitle}
         >
           <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+            integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+            crossorigin="anonymous"
+          />
+          <link
             href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css"
             rel="stylesheet"
           />
         </Helmet>
         <div className="container mx-auto sm:px-4 justify-center">
-          <div className="flex flex-col flex-wrap justify-between mt-8">
-            {orderBy(events, 'start_date', 'desc').map(event => (
-              <Event event={event} />
-            ))}
+          <Header />
+          <div class="py-4">
+            <h3 class="uppercase tracking-wide">Coming Up</h3>
+            <div className="flex flex-wrap justify-between mt-8">
+              {orderBy(upNext, 'start_date', 'desc').map(event => (
+                <Event event={event} />
+              ))}
+            </div>
+          </div>
+          <div class="py-4">
+            <h4 class="uppercase tracking-wide">Previously</h4>
+            <div className="flex flex-wrap justify-between mt-8">
+              {orderBy(previous, 'start_date', 'desc').map(event => (
+                <Event event={event} />
+              ))}
+            </div>
           </div>
         </div>
       </Layout>
@@ -57,6 +135,8 @@ export const pageQuery = graphql`
         slug
         start_date
         youtube_playlist
+        cover_path
+        event_type
       }
     }
   }
