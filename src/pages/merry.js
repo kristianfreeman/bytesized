e69edb1d1289f12ea2a033947149d2fb6e8385fe
Kristian Freeman <kristian@kristianfreeman.com>
2018-12-14 19:56:02 -0700

    Meta, baby

--- src/pages/merry.js
+++ src/pages/merry.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Helmet } from 'react-helmet'
 import Layout from '../components/Layout'
 
 class Merry extends React.Component {
@@ -11,6 +12,26 @@ class Merry extends React.Component {
   render = () => {
     return (
       <Layout>
+        <Helmet>
+          <title>A Very Merry Byteconf</title>
+          <meta property="og:title" content="A Very Merry Byteconf " />
+          <meta
+            name="description"
+            content="We're giving away over $2000 in web dev courses, subscriptions and more, to help you jump start your web dev career!
+"
+          />
+          <meta
+            property="og:description"
+            content="We're giving away over $2000 in web dev courses, subscriptions and more, to help you jump start your web dev career!"
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="https://byteconf-production.s3.amazonaws.com/headers/a_very_merry_byteconf.png"
+          />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:site" content="@byteconf" />
+        </Helmet>
         <div style={{ marginTop: '60px' }}>
           <a
             class="e-widget"
