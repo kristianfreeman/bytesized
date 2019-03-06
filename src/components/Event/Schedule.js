ad726c273a86072392f2c0685d9cb0413bda95c2
Kristian Freeman <kristian@kristianfreeman.com>
2019-03-06 10:34:30 -0600

    Grouped date and speaker shuffle

--- src/components/Event/Schedule.js
+++ src/components/Event/Schedule.js
@@ -1,25 +1,47 @@
 import * as React from 'react'
 
+import { DateTime } from 'luxon'
 import Talk from '../Talk'
-import { orderBy } from 'lodash'
+import { groupBy, orderBy } from 'lodash'
 
-const Schedule = ({ event }) => (
-  <div className="bg-black text-white text-center py-16">
-    <h3 className="text-3xl text-bold uppercase">Schedule</h3>
+const groupByDate = talks =>
+  groupBy(talks, talk =>
+    DateTime.fromISO(talk.time).toLocaleString(DateTime.DATE_HUGE)
+  )
 
-    <div className="my-6 max-w-lg mx-auto">
-      <h4 className="text-xl text-grey font-normal mb-4">
-        All times listed are in your browser's timezone,{' '}
-        {Intl.DateTimeFormat().resolvedOptions().timeZone}.
-      </h4>
+const Schedule = ({ event }) => {
+  const groupedTalks = groupByDate(event.talks)
+
+  return (
+    <div className="bg-black text-white text-center py-16">
+      <h3 className="text-3xl text-bold uppercase">Schedule</h3>
+
+      <div className="my-6 max-w-lg mx-auto">
+        <h4 className="text-xl text-grey font-normal mb-4">
+          All times listed are in your browser's timezone,{' '}
+          {Intl.DateTimeFormat().resolvedOptions().timeZone}.
+        </h4>
+      </div>
+
+      <div className="mt-12 mx-10">
+        {Object.keys(groupedTalks).map(key => {
+          return (
+            <div key={key}>
+              <h2>{key}</h2>
+              {orderBy(groupedTalks[key], 'position').map(talk => (
+                <Talk
+                  key={talk._id}
+                  company={false}
+                  social={false}
+                  talk={talk}
+                />
+              ))}
+            </div>
+          )
+        })}
+      </div>
     </div>
-
-    <div className="mt-12 mx-10">
-      {orderBy(event.talks, 'position').map(talk => (
-        <Talk key={talk._id} company={false} social={false} talk={talk} />
-      ))}
-    </div>
-  </div>
-)
+  )
+}
 
 export default Schedule
--- src/components/Event/Speakers.js
+++ src/components/Event/Speakers.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-
+import { shuffle } from 'lodash'
 import Speaker from '../Speaker'
 
 const EventSpeakers = ({ event }) => (
@@ -7,7 +7,7 @@ const EventSpeakers = ({ event }) => (
     <h3 className="text-3xl text-bold uppercase">Speakers</h3>
 
     <div className="flex flex-wrap mt-10 mx-10 justify-center">
-      {event.event_speakers.map(({ speaker }) => (
+      {shuffle(event.event_speakers).map(({ speaker }) => (
         <Speaker key={speaker._id} speaker={speaker} />
       ))}
     </div>
