8d30df677ce03bd8bcdf4e022658ee75f6b6459d
Kristian Freeman <kristian@kristianfreeman.com>
2019-11-14 12:10:53 -0600

    about links

--- src/system.js
+++ src/system.js
@@ -2,29 +2,62 @@ import React from 'react'
 import c from 'classnames'
 
 const Paragraph = ({ children, ...props }) => {
-  return <p className={c("measure-wide", "text-xl", "py-4")} {...props}>{children}</p>
+  return (
+    <p className={c('measure-wide', 'text-xl', 'py-4')} {...props}>
+      {children}
+    </p>
+  )
 }
 
-const TitleContainer = ({ alignment = "left", children }) => (
-  <div className={c("my-8", [{'text-align': `text-${alignment}`}])}>{children}</div>
+const Link = ({ children, ...props }) => {
+  return (
+    <a
+      className={c('text-orange-800 hover:text-orange-600 transition-all')}
+      {...props}
+    >
+      {children}
+    </a>
+  )
+}
+
+const TitleContainer = ({ alignment = 'left', children }) => (
+  <div className={c('my-8', [{ 'text-align': `text-${alignment}` }])}>
+    {children}
+  </div>
 )
 
 const Title = ({ alignment, children, size, ...props }) => {
-  const baseClasses = ["font-bold", "measure-narrow"]
+  const baseClasses = ['font-bold', 'measure-narrow']
   let tag
   switch (size) {
     case 'h1':
-      tag = <h1 {...props} className={c("text-4xl", "leading-tight", baseClasses)}>{children}</h1>
+      tag = (
+        <h1 {...props} className={c('text-4xl', 'leading-tight', baseClasses)}>
+          {children}
+        </h1>
+      )
       break
     case 'h2':
-      tag = <h2 {...props} className={c("text-3xl", baseClasses)}>{children}</h2>
+      tag = (
+        <h2 {...props} className={c('text-3xl', baseClasses)}>
+          {children}
+        </h2>
+      )
       break
     case 'h3':
-      tag = <h3 {...props} className={c("text-2xl")}>{children}</h3>
+      tag = (
+        <h3 {...props} className={c('text-2xl')}>
+          {children}
+        </h3>
+      )
       break
     case 'h4':
       break
-      tag = <h4 {...props} className={c("text-xl")}>{children}</h4>
+      tag = (
+        <h4 {...props} className={c('text-xl')}>
+          {children}
+        </h4>
+      )
     default:
       break
       tag = <p {...props}>{children}</p>
@@ -38,7 +71,8 @@ const System = {
   h2: props => <Title {...props} size="h2" />,
   h3: props => <Title {...props} size="h3" />,
   h4: props => <Title {...props} size="h4" />,
-  p: Paragraph
+  a: Link,
+  p: Paragraph,
 }
 
 export default System
